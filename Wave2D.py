0886286ec1afab3444118fab9bdc83732b6099cc
Ah-Aghabarari <ah.aghabarari@gmail.com>
2025-10-09 17:33:53 +0200

    Update Wave2D.py

--- Wave2D.py
+++ Wave2D.py
@@ -9,24 +9,45 @@ x, y, t = sp.symbols('x,y,t')
 class Wave2D:
 
     def create_mesh(self, N, sparse=False):
+        self.N = int(N)
+        self.L = 1.0
+        self.h = self.L / self.N
+        x1d = np.linspace(0.0, self.L, self.N + 1)
+        y1d = np.linspace(0.0, self.L, self.N + 1)
+        self.xij, self.yij = np.meshgrid(x1d, y1d, indexing="ij")
+
         """Create 2D mesh and store in self.xij and self.yij"""
         # self.xji, self.yij = ...
-        raise NotImplementedError
+        #raise NotImplementedError
 
     def D2(self, N):
+        m = self.N + 1
+        D2 = sparse.diags([1, -2, 1], [-1, 0, 1], (m, m), 'lil')
+        D2[0, :]  = 0.0
+        D2[-1, :] = 0.0
+        return (D2.tocsr())/(self.h**2)
         """Return second order differentiation matrix"""
-        raise NotImplementedError
+        #raise NotImplementedError
 
     @property
     def w(self):
+        return self.c * np.pi * np.sqrt(self.mx**2 + self.my**2)
+
         """Return the dispersion coefficient"""
-        raise NotImplementedError
+        #raise NotImplementedError
 
     def ue(self, mx, my):
+        return sp.sin(mx*sp.pi*x) * sp.sin(my*sp.pi*y) * sp.cos(self.w*t)
+
         """Return the exact standing wave"""
-        return sp.sin(mx*sp.pi*x)*sp.sin(my*sp.pi*y)*sp.cos(self.w*t)
+        #return sp.sin(mx*sp.pi*x)*sp.sin(my*sp.pi*y)*sp.cos(self.w*t)
 
     def initialize(self, N, mx, my):
+        U0 = np.sin(mx * np.pi * self.xij) * np.sin(my * np.pi * self.yij)
+        self.temp = U0.copy()
+        self.apply_bcs()
+        self.U = self.temp
+        self.Um1 = None 
         r"""Initialize the solution at $U^{n}$ and $U^{n-1}$
 
         Parameters
@@ -36,14 +57,19 @@ class Wave2D:
         mx, my : int
             Parameters for the standing wave
         """
-        raise NotImplementedError
+        #raise NotImplementedError
 
     @property
     def dt(self):
+        return self._dt
         """Return the time step"""
-        raise NotImplementedError
+        #raise NotImplementedError
 
     def l2_error(self, u, t0):
+        ue_fun = sp.lambdify((x, y, t), self.ue(self.mx, self.my), "numpy")
+        Ue = ue_fun(self.xij, self.yij, t0)
+        return np.sqrt((self.h**2) * np.sum((u - Ue) ** 2))
+
         """Return l2-error norm
 
         Parameters
@@ -53,12 +79,61 @@ class Wave2D:
         t0 : number
             The time of the comparison
         """
-        raise NotImplementedError
+        #raise NotImplementedError
 
     def apply_bcs(self):
-        raise NotImplementedError
+        U = self.temp
+        U[0, :] = U[-1, :] = 0.0
+        U[:, 0] = U[:, -1] = 0.0
+
+        #raise NotImplementedError
 
     def __call__(self, N, Nt, cfl=0.5, c=1.0, mx=3, my=3, store_data=-1):
+        self.mx, self.my, self.c = int(mx), int(my), float(c)
+        self.create_mesh(N)
+        self._dt = float(cfl) * self.h / self.c
+        Nt = int(Nt)
+        m = self.N + 1
+
+        D = self.D2(self.N)
+        I = sparse.identity(m, format="csr")
+        L = sparse.kron(I, D, format="csr") + sparse.kron(D, I, format="csr")
+
+        self.initialize(N, mx, my)
+
+        u0 = self.U.reshape(m * m, order="C")
+        Lu0 = L @ u0
+        Um1 = self.U + 0.5 * (self.c * self.dt) ** 2 * Lu0.reshape((m, m), order="C")
+        self.temp = Um1
+        self.apply_bcs()
+        self.Um1 = self.temp
+
+        errs = [self.l2_error(self.U, 0.0)]
+        out = None
+        if store_data is not None and store_data > 0:
+            out = {}
+            out[0.0] = self.U.copy() 
+        cdt = (self.c * self.dt) ** 2
+
+        for n in range(1, Nt + 1):
+            un = self.U.reshape(m * m, order="C")
+            Lun = L @ un
+            Unp1 = 2.0 * self.U - self.Um1 + cdt * Lun.reshape((m, m), order="C")
+
+            self.temp = Unp1
+            self.apply_bcs()
+            Unp1 = self.temp
+
+            self.Um1, self.U = self.U, Unp1
+
+            tn = n * self.dt
+            errs.append(self.l2_error(self.U, tn))
+            if out is not None and (n % store_data == 0):
+                out[tn] = self.U.copy()
+
+        if out is not None:
+            return out
+        return self.h, np.array(errs)
         """Solve the wave equation
 
         Parameters
@@ -83,7 +158,7 @@ class Wave2D:
         If store_data > 0, then return a dictionary with key, value = timestep, solution
         If store_data == -1, then return the two-tuple (h, l2-error)
         """
-        raise NotImplementedError
+        #raise NotImplementedError
 
     def convergence_rates(self, m=4, cfl=0.1, Nt=10, mx=3, my=3):
         """Compute convergence rates for a range of discretizations
@@ -121,13 +196,30 @@ class Wave2D:
 class Wave2D_Neumann(Wave2D):
 
     def D2(self, N):
-        raise NotImplementedError
+        def D2(self, N):
+        m = self.N + 1
+        D2 = sparse.diags([1, -2, 1], [-1, 0, 1], (m, m), 'lil')
+        D2[0, :]  = 0.0
+        D2[0, 0]  = -2.0
+        D2[0, 1]  =  2.0
+        D2[-1, :] = 0.0
+        D2[-1,-1] = -2.0
+        D2[-1,-2] =  2.0
+        return (D2.tocsr())/(self.h**2)
+        #raise NotImplementedError
 
     def ue(self, mx, my):
-        raise NotImplementedError
+        return sp.cos(mx*sp.pi*x) * sp.cos(my*sp.pi*y) * sp.cos(self.w*t)
+        #raise NotImplementedError
 
+    def initialize(self, N, mx, my):
+        U0 = np.cos(mx * np.pi * self.xij) * np.cos(my * np.pi * self.yij)
+        self.U = U0.copy()
+        self.Um1 = None
+        
     def apply_bcs(self):
-        raise NotImplementedError
+        return
+        #raise NotImplementedError
 
 def test_convergence_wave2d():
     sol = Wave2D()
@@ -140,4 +232,18 @@ def test_convergence_wave2d_neumann():
     assert abs(r[-1]-2) < 0.05
 
 def test_exact_wave2d():
-    raise NotImplementedError
+    C = 1.0 / np.sqrt(2.0)
+    mx = my = 2
+    N = 64
+    Nt = 128
+
+    # Dirichlet
+    sol = Wave2D()
+    h, E = sol(N, Nt, cfl=C, c=1.0, mx=mx, my=my, store_data=-1)
+    assert np.max(np.abs(E)) < 1e-12
+
+    # Neumann
+    solN = Wave2D_Neumann()
+    h, E = solN(N, Nt, cfl=C, c=1.0, mx=mx, my=my, store_data=-1)
+    assert np.max(np.abs(E)) < 1e-12
+    #raise NotImplementedError
